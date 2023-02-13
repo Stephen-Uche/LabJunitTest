061efb0e6ceabaa53c0ede57ef4e78634d254141
Osedumme Steven <stephen.osedumme@iths.se>
2023-02-13 11:03:34 +0100

    Created TDDTest

---
+++ src/test/java/com/example/gaming/GameTDDTest.java
@@ -0,0 +1,24 @@
+package com.example.gaming;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameTDDTest {
+
+    private GameTDD gameTDD;
+
+    @BeforeEach
+    void setUp() {
+
+        gameTDD = new GameTDD();
+
+    }
+
+    @Test
+    void spareIsToKnockDownAllPinsWithTwoRolls() {
+
+        //Given
+        //When
+        //Then
+    }
+}
